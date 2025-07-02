2b5cfb1d9f1ece8eb3cf5aad047af97eed79c6b9
petronela <delyoara@yahoo.fr>
2025-07-02 11:49:20 +0200

    fix: resolved issue with the volunteers route (restored missing logic)

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from .views import create_adopter
-from .views import  liste_adopters, liste_animaux
+from .views import  liste_adopters, liste_animaux, liste_volunteers
 
 urlpatterns = [
     path("adopter/", create_adopter, name="create_adopter"),
     path("adopters/", liste_adopters, name="liste_adopters"),
     path("animaux/", liste_animaux, name="liste_animaux"),
+    path("volunteers/", liste_volunteers, name="liste_volunteers"),
 ]
-# urlpatterns = [
-#     path("adopter/", create_adopter, name="create_adopter"),
-#       path("api/adopters/", liste_adopters, name="liste_adopters"),
-# ]
+
