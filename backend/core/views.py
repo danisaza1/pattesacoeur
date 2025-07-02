877e0ac4362ab692a2a1b2f85c8d701035daf168
rerefenec <renaud.delorme@free.fr>
2025-07-02 11:22:35 +0200

    modification de views.py, package-lock.json,package.json

--- backend/core/views.py
+++ backend/core/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.contrib.auth.hashers import make_password
 from django.views.decorators.csrf import csrf_exempt
-from core.models import Animal, Volunteer
+from core.models import Animal, Volunteer, Adopter
 import json
 
 @csrf_exempt
@@ -106,3 +106,49 @@ def liste_volunteers(request):
             return JsonResponse({'error': str(e)}, status=500)
     else:
             return JsonResponse({'error': 'Méthode non autorisée'}, status=405)
+
+
+@csrf_exempt
+def create_adopter(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            required_fields = ["firstname", "lastname", "email", "password", "location"]
+            for field in required_fields:
+                if field not in data or not data[field]:
+                    return JsonResponse({"error": f"Le champ {field} est requis."}, status=400)
+
+            adopter = Adopter.objects.create(
+                firstname=data["firstname"],
+                lastname=data["lastname"],
+                email=data["email"],
+                password=data["password"],  # Pense à hasher le mot de passe en prod !
+                location=data["location"],
+            )
+            return JsonResponse({"message": "Adopteur créé", "id": adopter.id})
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "JSON invalide"}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+    else:
+        return JsonResponse({"error": "Méthode non autorisée"}, status=405)
+    
+@csrf_exempt
+def liste_adopters(request):
+    if request.method == "GET":
+        adopters = Adopter.objects.all()  # <- attention à .objects et pas .object
+        data = []
+
+        for adopter in adopters:  # <- tu avais oublié la boucle
+            adopter_dict = {
+                'id': adopter.id,
+                'prenom': adopter.firstname,
+                'nom': adopter.lastname,
+                'email': adopter.email,
+                'location': adopter.location,
+            }
+            data.append(adopter_dict)
+
+        return JsonResponse(data, safe=False)
+    else:
+        return JsonResponse({"error": "Méthode non autorisée"}, status=405)
