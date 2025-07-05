c96f7e32b555b35a7ba683c3e73f0a5c9c468193
petronela <delyoara@yahoo.fr>
2025-07-05 15:43:18 +0200

    Connected volunteer account to authentication system

--- frontend/app/components/volunteerAdopter.tsx
+++ frontend/app/components/volunteerAdopter.tsx
@@ -10,7 +10,7 @@ export default function VolunteerAdopter() {
         <div className="  width: 1rem  flex  flex-col md:flex-row items-center justify-center gap-32  w-1/2  gap-6 md:gap-8 w-full max-w-4xl mx-auto">
           
           
-          <Link href="/comptevolunteer">
+          <Link href="/volunteer/login">
           <button className="px-22 py-6 text-lg font-bold text-white bg-[#324960] rounded-lg shadow-[0_4px_0_0_rgba(0,0,0,0.2)] hover:bg-[#6da2b5] hover:shadow-[0_6px_12px_rgba(6,182,212,0.4)] active:translate-y-1 active:shadow-[0_2px_0_0_rgba(0,0,0,0.2)] transition-all duration-200 ease-in-out whitespace-nowrap cursor-pointer">
             Benevole
           </button>
