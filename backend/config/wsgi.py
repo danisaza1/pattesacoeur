feaf9e89379795117589b6d97802cb32e506041f
danisaza1 <dani_isaza@htomail.com>
2025-07-16 10:19:12 +0200

    initial django commit

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Ajoute le dossier parent à sys.path pour que Django trouve ton projet
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
